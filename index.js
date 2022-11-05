12ae25b6bbee62cf5cbdbb200303a4941337c46d
aimeric rouyer <aimeric.rouyer@epitech.eu>
2022-11-05 22:29:36 +0100

    making the site public

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const fs = require('fs');
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static('DMWEB/public'))
+app.get('/', function (req, res) {
+   res.sendFile('DMWEB/accueil.html', { root: __dirname })
+})
+app.get('/', function (req, res) {
+   res.sendFile('DMWEB/paiement.html', { root: __dirname })
+})
+app.listen(3000, function () {
+ console.log('Votre app est disponible sur localhost:3000 !')
+})
